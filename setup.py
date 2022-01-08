88f33ce371d8d17a154e70cd44760044307a0190
adler72 <53665786+adler72@users.noreply.github.com>
2022-01-08 11:22:25 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='cbpi4-PIDI2C',
-      version='0.0.7',
+      version='0.0.8',
       description='CraftBeerPi4 PID Kettle Logic Plugin',
       author=['Marc Adler'],
       author_email='aeda@gmx.de',
